1497aef7a242dfe73f93260460ff7f2ff2ba9477
Davanesh <davaneshsaminatan335@gmail.com>
2024-10-25 20:41:47 +0530

    errors

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,5 +13,5 @@ export default defineConfig({
     },
   },
 
-  plugins: [react()],
+  plugins: [react(), nodeResolve()]
 })
